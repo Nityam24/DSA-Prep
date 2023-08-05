71304da19fa55c0ab539e96e508ee4d996d80742
Nityam24 <nityampatel2402@gmail.com>
2023-08-05 18:53:56 +0530

    new update

---
+++ dsaSheet.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	char val;
+	cout<<"enter the value"<<endl;
+	cin>>val;
+
+	// if(val<="a" && val>="z"){
+	// 	cout<<"0"<<endl;
+	// }	
+	// else if(val<="A" && val>="Z"){
+	// 	cout<<"1"<<endl;
+	// }
+	// else{
+	// 	cout<<"-1"<<endl;
+	// }
+
+    cout<<int(val);
+	return 0;
+}
