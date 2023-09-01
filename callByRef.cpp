a7197e9b9bdaa48b2a7f2e4cbb7247b2ba03b915
Nityam24 <nityampatel2402@gmail.com>
2023-09-01 23:54:48 +0530

    new update

---
+++ callByRef.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int Maximum(int x, int y)
+{
+	if(x>y)return x;
+	else if(y>x)return y;
+	else return x || y;
+}
+void Swap(int &x, int &y)
+{
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+int main()
+{
+	int test, a, b, r;
+	cin >> test;
+	cin >> a >> b;
+	switch (test)
+	{
+	case 1:
+		r = Maximum(a, b);
+		cout << r;
+		break;
+	case 2:
+		Swap(a, b);
+		cout << a << " " << b;
+		break;
+	default:
+		cout << "Invalid test option";
+	}
+	return 0;
+}
