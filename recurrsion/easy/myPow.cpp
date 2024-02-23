133691edc3c97ad04b4c01e61e0ebc2cf5e6e8b3
Nityam24 <nityampatel2402@gmail.com>
2024-02-23 10:50:37 +0530

    update

--- recurrsion/easy/myPow.cpp
+++ recurrsion/easy/myPow.cpp
@@ -25,3 +25,22 @@ public:
         }
     }
 };
+
+
+//optimize way
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(n<0) {
+            n = abs(n);
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else{
+            return x*myPow(x, n-1);
+        }
+    }
+};
